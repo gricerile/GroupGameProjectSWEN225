ca06090c7396182ca0da5138e6ce3884d48f3f31
Riley Grice <gricerile@cafe-india.ecs.vuw.ac.nz>
2018-10-03 12:40:10 +1300

    added a plan for main when the gui is complete for me to use

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
 	ArrayList<Segment> segments = new ArrayList<Segment>();
 	Player player = new Player(0,0);
 
+	//onClick events
+	//on game objects
 
+
+	//button events forward, back, left, right
+	//moving player's perspective
+
+
+	//keyboard events WASD/wasd
+	//moving players perspective
 }
