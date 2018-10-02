2c5e8d0bc504ac356cec2d6cd69685c7ae51425d
Kelvin Chen <chenkelv@ecs.vuw.ac.nz>
2018-10-02 22:27:02 +1300

    Update Texture.java, I followed a tutorial in rendering, there was an issue I had with getting the walls to render, image pixel sizes do matter otherwise errors will be thrown

--- src/Texture.java
+++ src/Texture.java
@@ -1,48 +1,33 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
 
-
 public class Texture {
-  public ArrayList<Texture> textures;   // let other classes access the texture files
-  public int[] pixels;           // hold the data for all pixels in the image of texture
-  private String fileLocation;   // image file location
-  public int size;               // texture image size
-
-  /**
-   * Tecture constructor.
-   * @param location of the file.
-   * @param size of the file eg 64x64.
-   */
-  public Texture(String location, int size) {
-    this.fileLocation = location;
-    this.size = size;
-    pixels = new int [size * size];
-    load();
-  }
-
-  /**
-   * loading in texture files.
-   */
-  private void load() {
-    try {
-      BufferedImage image = ImageIO.read(new File(fileLocation));
-      int w = image.getWidth();
-      int h = image.getHeight();
-      image.getRGB(0, 0, w, h, pixels, 0, w);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  // get some texture files to load in.
-
-  public static Texture floor = new Texture();
-  public static Texture wall = new Texture();
-  public static Texture ceilling = new Texture();
-
-
+	public int[] pixels;
+	private String loc;
+	public final int SIZE;
+	
+	public Texture(String location, int size) {
+		loc = location;
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		load();
+	}
+	
+	private void load() {
+		try {
+			BufferedImage image = ImageIO.read(new File(loc));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Texture wood = new Texture("res/sky.png", 65);        // That value represents the pixel size, so the image needs to be symmetrical
+	public static Texture brick = new Texture("sad/redbrick.jpg", 64);
+	public static Texture bluestone = new Texture("sad/bluestone.jpg", 64);
+	public static Texture stone = new Texture("sad/greystone.jpg", 64);
 }
