4c936cb037a12af5a3bf17ff88be63afcff2741a
Riley Grice <gricerile@ecs.vuw.ac.nz>
2018-10-03 15:37:25 +1300

    Update Segment.java

--- src/Segment.java
+++ src/Segment.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
 
 public class Segment {
+	//fields
+	Door door1;
+	Door door2;
+	ArrayList<GameObject> objects = new ArrayList<GameObject>();
+
+	public Segment(ArrayList<GameObject> objects,Door d1, Door d2) {
+		this.objects=objects;
+		this.door1=d1;
+		this.door2=d2;
+	}
+
 
 }
