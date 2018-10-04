0271e7cdaa6e9292f189dffdd1510dd69851fa44
Riley Grice <gricerile@poquito.ecs.vuw.ac.nz>
2018-10-04 14:03:57 +1300

    adding methods and logic to main, such as move methods with one helper method, door, segment have also been updated to help methods in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,67 +8,84 @@ import renderer.Renderer;
 import GUI.GUI.moveDirection;
 
 public class Main {
-  private Segment[][] segmentsBoard = new Segment[2][1]; // the size can change but for testing the size is 2 segments
-  private Player player;
-  private GUI g;
+	private Segment[][] segmentsBoard = new Segment[2][1]; // the size can change but for testing the size is 2 segments
+	private Player player;
+	private GUI g;
 
-  private Renderer renderer;
+	private Renderer renderer;
 
-  public static void main(String[] args) {
-    Main m = new Main();
-  }
+	public static void main(String[] args) {
+		Main m = new Main();
+	}
 
-  public Main() {
-    Parser p = new Parser();
-    p.loadMap(new File("ParsingTester.xml"));
-    // this.segmentsBpoard=p.getSegments(); will be created
-    // this.player=p.getPlayer(); will be created, lets do this
-    this.g = new GUI(this);
-    this.renderer= new Renderer();
-  }
+	public Main() {
+		Parser p = new Parser();
+		p.loadMap(new File("ParsingTester.xml"));
+		// this.segmentsBpoard=p.getSegments(); will be created
+		// this.player=p.getPlayer(); will be created, lets do this
+		this.g = new GUI(this);
+		this.renderer = new Renderer();
+	}
 
-  public Segment[][] getSegments() {
-    return this.segmentsBoard;
-  }
+	public Segment[][] getSegments() {
+		return this.segmentsBoard;
+	}
 
-  public Player getPlayer() {
-    return this.player;
-  }
+	public Player getPlayer() {
+		return this.player;
+	}
 
-  public GUI getGUI() {
-    return this.g;
-  }
+	public GUI getGUI() {
+		return this.g;
+	}
 
-  public Renderer getRenderer() {
-    return this.renderer;
-  }
+	public Renderer getRenderer() {
+		return this.renderer;
+	}
 
-  public void movePlayer(moveDirection direction) {
-    reDraw();
-  }
+	public void movePlayer(moveDirection direction) {
+		if (canMove(direction)) {
+			if (!player.getSegment().getDoor(direction).getSeg1().equals(player.getSegment())) {
+				player.move(player.getSegment().getDoor(direction).getSeg1());
+			} else {
+				player.move(player.getSegment().getDoor(direction).getSeg2());
+			}
+		}
+		reDraw();
+	}
 
-  public void clickedScreen(int x, int y) {
-    reDraw();
-  }
+	public boolean canMove(moveDirection direction) {
+		if (player.getSegment().hasDoor(direction)) {
+			if (player.getSegment().getDoor(direction) != null
+					&& player.getSegment().getDoor(direction).getOpen() == true) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  public void pressedKey() {
+	public void clickedScreen(int x, int y) {
+		reDraw();
+	}
 
-  }
+	public void pressedKey() {
 
-  public void saveGame() {
+	}
 
-  }
+	public void saveGame() {
 
-  public void loadGame() {
+	}
 
-  }
+	public void loadGame() {
 
-  public void quitGame() {
+	}
 
-  }
+	public void quitGame() {
 
-  public void reDraw() {
-    this.g.getFrame().getGraphicsWindow().redraw();
-  }
+	}
+
+	public void reDraw() {
+		this.g.getFrame().getGraphicsWindow().redraw();
+	}
 
 }
