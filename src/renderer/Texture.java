7e4cffa27099e201c2de42c3fb27e66e65b776e3
Kelvin Chen <chenkelv@ecs.vuw.ac.nz>
2018-10-04 21:19:34 +1300

    Texture.java is use to load in the textures from the tx folder, have not merged this class into any of the other classes yet

---
+++ src/renderer/Texture.java
@@ -0,0 +1,40 @@
+package renderer;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Texture {
+	public int[] pixels;			// hold data of the image
+	private String fileLocation;	// path to image
+	public final int textureSize;	// size of the texture 
+	
+	public Texture(String fileLocation, int textureSize) {
+		this.fileLocation = fileLocation;
+		this.textureSize = textureSize;
+		this.pixels = new int[textureSize * textureSize];
+		
+		// call the onLoad method
+		onLoad();
+	}
+	
+	public void onLoad() {
+		try {
+			BufferedImage image = ImageIO.read(new File(fileLocation));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	
+	
+	// set up the different textures
+	public static Texture grass = new Texture("tx/grass.jpg", 65);
+	public static Texture wall = new Texture("tx",65);
+	
+}
