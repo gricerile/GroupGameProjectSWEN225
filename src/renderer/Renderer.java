8f2b6c8036425cd5ed7f796b47dc6b79d1507a00
Kelvin Chen <chenkelv@ecs.vuw.ac.nz>
2018-10-04 21:18:16 +1300

    Update Renderer.java, trying to get the 2D array to render an image rather than just drawing rectangles

--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -1,8 +1,17 @@
 package renderer;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.awt.image.ImageObserver;
 
+import javax.swing.ImageIcon;
+
+import GUI.GUI;
+import GUI.GameFrame;
 import main.Main;
 import main.Segment;
 
@@ -11,35 +20,54 @@ public class Renderer {
   private Main m;
   private Segment[][] board;
   public enum Item{EMPTY, DOOR, CHEST};
-  private int sz = 10;
+  private int sz = 20;
   
-  private int colCount = 10;
-  private int rowCount = 10;
+  private int SEG_WIDTH = 20;
+  private int SEG_HEIGHT = 20;
   
+  private Image image;
   
+  private GameFrame gf;
   
   public Renderer() {
 //	  this.board = m.getSegments();	// initialise the board with the 2D array of segs stored in the main class
-	  
+	  this.board = new Segment[10][10];
   }
 
+  private void loadImage() {
+	  ImageIcon i = new ImageIcon("tx/grass.jpg");
+	  image = i.getImage();
+  }
   
   public void draw(Graphics g, int windowWidth, int windowHeight) {
-	  this.board = new Segment[20][20];
-	  for (int i = 0; i < board.length; i++)
-			for(int j = 0; j<board[i].length; j++)
-				g.drawRect(i*sz, j*sz, sz, sz);
-				//g.drawRect(sz/2*i+sz/2*j, sz/2*i+sz/2*j, sz, sz);
-
-//    g.drawRect((windowWidth/2)-50, (windowHeight/2)-50, 100, 100);
-//    g.drawOval((windowWidth/2)-50, (windowHeight/2)-50, 100, 100);
+	  
+	  for (int i = 0; i < board.length; i++) {
+		  for(int j = 0; j<board[i].length; j++) {
+				g.drawRect((windowWidth/2) -i*SEG_WIDTH,(windowHeight/2) -  j*SEG_HEIGHT, sz, sz);
+				// g.drawImage(board[i][j].getTexture(), (windowWidth/2) -i*SEG_WIDTH, (windowHeight/2) -  j*SEG_HEIGHT, m.getGUI());
+		  }
+		  
+	  }
   }
+  
+  
+  public void updateBoard() {}
+  
+  public 
   
   
   // turning regular 2d array into isometric
   // isoX = cartX - cartY;
   // isoY = (cartX + cartY) / 2
   public void toIso() {
-	  
+	  for (int i = 0; i < board.length; i++) {
+		  for(int j = 0; j<board[i].length; j++) {
+			  
+			  // screen.x = map.x * SEG_HEIGHT
+			  // screen.y = map.y * SEG_WIDTH
+		  }
+	  }
   }
+  
+
 }
