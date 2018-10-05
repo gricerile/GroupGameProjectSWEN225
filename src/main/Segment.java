83238bcd79e5ec895d2a8243a8c6b16cf70380f4
Riley Grice <gricerile@poquito.ecs.vuw.ac.nz>
2018-10-05 14:53:07 +1300

    changes to segment and main, main has pseudocode for load and save. and can move now has null checks to make sure null pointers are caught and no errors are thrown.

--- src/main/Segment.java
+++ src/main/Segment.java
@@ -1,14 +1,29 @@
 package main;
 
 public class Segment {
-  // fields
-  private GameObject object;
+	//fields
+	private GameObject object;
+	private int id;
 
-  public Segment(GameObject object, int id) {
-    this.object = object;
-  }
+	public Segment(GameObject object, int id) {
+		this.object=object;
+		this.id=id;
+	}
 
-  public GameObject getObject() {
-    return this.object;
-  }
+	public GameObject getObject(){
+		return this.object;
+	}
+
+	public int getID() {
+		return this.id;
+	}
+
+	public boolean equals(Segment s) {
+		if(this.id==s.getID()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
+
