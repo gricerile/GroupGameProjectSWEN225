588c5195f1abf7f30d31100d9d631d232d3a6dd3
Kelvin Chen <chenkelv@gare-fare.ecs.vuw.ac.nz>
2018-10-09 14:37:55 +1300

    this is the renderer that draws using jpgs

--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -6,115 +6,93 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.ImageObserver;
-import java.util.ArrayList;
+import javafx.scene.shape.*;
 
+//import javafx.application.Application;
+//import javafx.scene.Group;
+//import javafx.scene.Scene;
+//import javafx.scene.shape.Polygon;
+//import javafx.stage.Stage;
 import javax.swing.ImageIcon;
-
 
 import GUI.GUI;
 import GUI.GUI.moveDirection;
 import GUI.GameFrame;
 import main.Main;
 import main.Segment;
-import javafx.scene.shape.*;
 
-
-
-
-public class Renderer {
+public class Renderer{
 
   private Main m;
   private main.Segment[][] board;
+  public enum Item{EMPTY, DOOR, CHEST};
   private int sz = 20;
 
-  private int SEG_WIDTH = 40;
-  private int SEG_HEIGHT = 40;
-
-  //private ArrayList<Polygon> allPolygons = new ArrayList<>();
+  private int SEG_WIDTH = 20;
+  private int SEG_HEIGHT = 20;
 
   private Image image;
+  public int isox;
+  public int isoy;
+
+//  public Polygon isoTile;
+
 
   public Renderer(Main main) {
       this.m = main;
 	  this.board = m.getSegments();	// initialise the board with the 2D array of segs stored in the main class
 	  //this.board = new Segment[10][10];
 
+	  Texture t = new Texture();
+	  this.image = t.onLoad("/tx/grass.png");
   }
 
-  private void loadImage() {
-	  ImageIcon i = new ImageIcon("/GroupProject/src/tx/grass.jpg");
-	  image = i.getImage();
-  }
+
+
 
   public void draw(Graphics g, int windowWidth, int windowHeight) {
+//	  isoTile = new Polygon();
+//	  isoTile.getPoints().addAll(new Double[] {
+//		0.0, 0.0,
+//		20.0, 20.0,
+//		0.0, 20.0,
+//		0.0, 0.0
+//	  });
+	  //toIso();
 
 
-	 // drawPlayer(g);
 
-//      Texture t = new Texture();
-//	  Image img = t.onLoad("/tx/dirt.png");
-//	  g.drawImage(img, 10, 10, null);
-//
-//	  Image character = t.onLoad("/tx/grass.png");
-//	  g.drawImage(character, 100, 100, null);
-//
-//
-//
-//	  int startX = windowWidth/2;
-//	  int startY = windowHeight/2;
-//	  for (int i = 0; i < board.length; i++) {
-//		  for(int j = 0; j<board[i].length; j++) {
-//				g.drawLine(startX, startY, startX+(SEG_WIDTH/2), startY+(SEG_HEIGHT/3));
-//				startX += SEG_WIDTH/2;
-//				startY += SEG_HEIGHT/3;
-//		  }
-//          startX = windowWidth/2 - (SEG_WIDTH*(i+1))/2;
-//		  startY = windowHeight/2 + (SEG_HEIGHT*(i+1))/3;
-//	  }
-//	  for (int i = 0; i < board.length; i++) {
-//          g.drawLine(startX, startY, startX + SEG_WIDTH/2, startY + SEG_HEIGHT/3);
-//          startX += SEG_WIDTH/2;
-//          startY += SEG_HEIGHT/3;
-//      }
-//
-//      startX = windowWidth/2;
-//      startY = windowHeight/2;
-//      for (int i = 0; i < board.length; i++) {
-//          for(int j = 0; j<board[i].length; j++) {
-//              g.drawLine(startX, startY, startX - (SEG_WIDTH/2), startY + (SEG_HEIGHT/3));
-//              startX -= SEG_WIDTH/2;
-//              startY += SEG_HEIGHT/3;
-//          }
-//          startX = windowWidth/2 + (SEG_WIDTH*(i+1))/2;
-//          startY = windowHeight/2 + (SEG_HEIGHT*(i+1))/3;
-//      }
-//      for (int i = 0; i < board.length; i++) {
-//
-//	          g.drawLine(startX, startY, startX - SEG_WIDTH/2, startY + SEG_HEIGHT/3);
-//	          startX -= SEG_WIDTH/2;
-//	          startY += SEG_HEIGHT/3;
-//      }
 
-	  for(int i = 0; i < board.length; i++) {
-		  for(int j = 0; j < board[i].length; j++) {
-			  g.drawRect((windowWidth/2) - i*SEG_WIDTH, (windowHeight/2) - j*SEG_HEIGHT, SEG_WIDTH, SEG_HEIGHT);
-			  // g.drawPolygon(new int[] {0, 20, 40, 20}, new int[] {10, 0, 10, 20}, 4);
 
+//	  AffineTransform at = AffineTransform.getShearInstance(0.5, -0.5);
+//	  Graphics2D g3d = (Graphics2D) g;
+//	  g3d.transform(at);
+
+	  for(int i = board.length; i > 0 ; --i) {
+		  for(int j = board[0].length-1; j >= 0; j--) {
+
+
+			  //g.drawRect(SEG_WIDTH/2*i+SEG_HEIGHT/2*j, SEG_WIDTH/2*i+SEG_HEIGHT/2*j, sz, sz);
+			  g.drawImage(image, (windowWidth/2) - isoX(i, j),  (windowHeight/2) - isoY(i, j), null);
+			  // g.drawRect((windowWidth/2) -i*SEG_WIDTH,(windowHeight/2) -  j*SEG_HEIGHT, sz, sz);
+			  //g.drawImage(img, (windowWidth/2) - ((i-j) *segWidth/2), (windowHeight/2) - ((i + j)*segHeight/2), null);
 
 		  }
 	  }
+
+
+
       	// there is a wall fill the rectangle to indicate that it is a wall
-      	if(checkWall()) {
-      		g.fillRect(10, 10, 10, 10);
-      	}
+//      	if(checkWall()) {
+//      		g.fillRect(10, 10, 10, 10);
+//      	}
+
 
   }
 
-//  public void drawPlayer() {
-//	  m.getPlayer().loadImage();
-//  }
 
   public boolean checkWall() {
 	  for(int i = 0; i < board.length; i++) {
@@ -131,25 +109,36 @@ public class Renderer {
   }
 
 
-  public void updateBoard() {}
 
 
   // turning regular 2d array into isometric
   // isoX = cartX - cartY;
   // isoY = (cartX + cartY) / 2
-  public void toIso() {
-	  for (int i = 0; i < board.length; i++) {
-		  for(int j = 0; j<board[i].length; j++) {
+	private int isoX(int x, int y) {
+		// convert x into isometric x
+		  int segWidth = image.getWidth(null);
 
-			  // screen.x = map.x * SEG_HEIGHT
-			  // screen.y = map.y * SEG_WIDTH
+		int isoX;
+		isoX = (int) ((x * segWidth / 2) - (y * segWidth / 2));
+		return isoX;
+	}
+
+	private int isoY(int x, int y) {
+		// convert y into isometric y
+		int segHeight = image.getHeight(null)-15;
+		int isoY;
+		isoY = (int) ((x * segHeight / 2) + (y * segHeight / 2));
+
+		return isoY;
+	}
+
+	public void convert(Segment[][] board) {
+	  for(int i = 0; i < board.length; i++) {
+		  for(int j = 0; j < board[i].length; j++) {
+
 		  }
 	  }
   }
-
-
-
-
 
   /*each square in a 2d grid is an segment object
    * get each segment and use coords to use in polygons to draw the new isometric square
