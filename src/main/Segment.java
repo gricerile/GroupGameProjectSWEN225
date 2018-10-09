389feffd081b2aedef5a05bb79c472d03d9ce630
Riley Grice <gricerile@ecs.vuw.ac.nz>
2018-10-09 13:32:54 +1300

    Update Segment.java

--- src/main/Segment.java
+++ src/main/Segment.java
@@ -3,23 +3,28 @@ package main;
 public class Segment {
 	//fields
 	private GameObject object;
-	private int id;
+	private int x,y;
 
-	public Segment(GameObject object, int id) {
+	public Segment(GameObject object, int x,int y) {
 		this.object=object;
-		this.id=id;
+		this.x=x;
+		this.y=y;
 	}
 
 	public GameObject getObject(){
 		return this.object;
 	}
 
-	public int getID() {
-		return this.id;
+	public int getX() {
+		return this.x;
+	}
+
+	public int getY() {
+		return this.y;
 	}
 
 	public boolean equals(Segment s) {
-		if(this.id==s.getID()) {
+		if(this.getX()==s.getX() && this.getY()==s.getY()) {
 			return true;
 		} else {
 			return false;
