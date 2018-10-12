df92d76dca11f3e406f0568216b6cc4d4baecec4
Daniel Miller <daniel.miller.djm@gmail.com>
2018-10-12 18:05:49 +1300

    linked inventory system in GUI to player inventory, to display current state of players inventory

--- src/GUI/InventoryPanel.java
+++ src/GUI/InventoryPanel.java
@@ -25,106 +25,111 @@ import main.Key;
 @SuppressWarnings("serial")
 public class InventoryPanel extends JPanel {
 
-  private int dimensionHeight = 120;
-  private int splitPaneWidth = 600;
-  private int splitPaneHeight = 80;
-  private String panelName = "Inventory";
+	private int dimensionHeight = 120;
+	private int splitPaneWidth = 600;
+	private int splitPaneHeight = 80;
+	private String panelName = "Inventory";
 
-  private GameFrame frame;
+	private GameFrame frame;
 
-  JScrollPane scrollPane;
-  JList<GameItem> listOfItems;
-  DefaultListModel<GameItem> listModel;
+	JScrollPane scrollPane;
+	JList<GameItem> listOfItems;
+	DefaultListModel<GameItem> listModel;
 
-  JSplitPane splitPane;
+	JSplitPane splitPane;
 
-  JPanel rightPanel;
-  JTextArea itemDescriptions;
+	JPanel rightPanel;
+	JTextArea itemDescriptions;
 
-  /**
-   * InventoryPanel stores a list and a text box. The list lists each item in the
-   * players inventory, and when an item is clicked on the text box displays a
-   * Description of that item.
-   *
-   * @param frame
-   *          instance of GameFrame which this class is contained in, encase class
-   *          needs to reference back.
-   */
-  public InventoryPanel(GameFrame frame) {
-    this.frame = frame;
-    this.setBackground(new Color(51, 153, 255));
+	/**
+	 * InventoryPanel stores a list and a text box. The list lists each item in the
+	 * players inventory, and when an item is clicked on the text box displays a
+	 * Description of that item.
+	 *
+	 * @param frame
+	 *            instance of GameFrame which this class is contained in, encase
+	 *            class needs to reference back.
+	 */
+	public InventoryPanel(GameFrame frame) {
+		this.frame = frame;
+		this.setBackground(new Color(51, 153, 255));
 
-    // set dimensions
-    Dimension dimension = getPreferredSize();
-    dimension.height = dimensionHeight;
-    setPreferredSize(dimension);
-    setMaximumSize(new Dimension(Integer.MAX_VALUE, dimensionHeight));
-    setMinimumSize(new Dimension(Integer.MIN_VALUE, dimensionHeight));
+		// set dimensions
+		Dimension dimension = getPreferredSize();
+		dimension.height = dimensionHeight;
+		setPreferredSize(dimension);
+		setMaximumSize(new Dimension(Integer.MAX_VALUE, dimensionHeight));
+		setMinimumSize(new Dimension(Integer.MIN_VALUE, dimensionHeight));
 
-    // set border
-    //setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), panelName));
-    setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+		// set border
+		// setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+		// panelName));
+		setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
-    //////////////////////////// Add Elements///////////////////////////////////
+		//////////////////////////// Add Elements///////////////////////////////////
 
-    // JList (left section of JSplitPane)
+		// JList (left section of JSplitPane)
 
-    listOfItems = new JList<>();
-    listModel = new DefaultListModel<>();
+		listOfItems = new JList<>();
+		listModel = new DefaultListModel<>();
 
-    listOfItems.setModel(listModel);
-    listOfItems.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-    listOfItems.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-    listOfItems.setVisibleRowCount(-1);
+		listOfItems.setModel(listModel);
+		listOfItems.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		listOfItems.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		listOfItems.setVisibleRowCount(-1);
 
-    scrollPane = new JScrollPane(listOfItems);
+		scrollPane = new JScrollPane(listOfItems);
 
-    // JPanel containing Jlabel and JTextArea (right section of JSplitPane)
+		// JPanel containing Jlabel and JTextArea (right section of JSplitPane)
 
-    itemDescriptions = new JTextArea();
-    itemDescriptions.setLineWrap(true);
-    itemDescriptions.setEditable(false);
+		itemDescriptions = new JTextArea();
+		itemDescriptions.setLineWrap(true);
+		itemDescriptions.setEditable(false);
 
-    // JLabel image = new JLabel(GUI.resizeImage("arrowImages/arrowDownLeft.png",
-    // 50, 50));
+		// JLabel image = new JLabel(GUI.resizeImage("arrowImages/arrowDownLeft.png",
+		// 50, 50));
 
-    // rightPanel = new JPanel();
-    // rightPanel.setLayout(new BorderLayout());
+		// rightPanel = new JPanel();
+		// rightPanel.setLayout(new BorderLayout());
 
-    // rightPanel.add(image, BorderLayout.WEST);
-    // rightPanel.add(itemDescriptions, BorderLayout.CENTER);
+		// rightPanel.add(image, BorderLayout.WEST);
+		// rightPanel.add(itemDescriptions, BorderLayout.CENTER);
 
-    // JSPlitPane
-    splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, itemDescriptions);
-    splitPane.setOneTouchExpandable(true);
-    splitPane.setDividerLocation(splitPaneWidth / 2);
-    splitPane.setPreferredSize(new Dimension(splitPaneWidth, splitPaneHeight));
+		// JSPlitPane
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, itemDescriptions);
+		splitPane.setOneTouchExpandable(true);
+		splitPane.setDividerLocation(splitPaneWidth / 2);
+		splitPane.setPreferredSize(new Dimension(splitPaneWidth, splitPaneHeight));
 
-    // set layout
-    setLayout(new GridBagLayout());
-    GridBagConstraints grid = new GridBagConstraints();
+		// set layout
+		setLayout(new GridBagLayout());
+		GridBagConstraints grid = new GridBagConstraints();
 
-    // set components
+		// set components
 
-    grid.gridx = 0;
-    grid.gridy = 0;
-    add(splitPane, grid);
+		grid.gridx = 0;
+		grid.gridy = 0;
+		add(splitPane, grid);
 
-    // add listener
-    listOfItems.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+		// add listener
+		listOfItems.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
-      @Override
-      public void valueChanged(ListSelectionEvent e) {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
 
-        GameItem object = listOfItems.getSelectedValue();
-        itemDescriptions.setText(object.getDescription());
-        // image.setIcon(ob.getImage());
+				GameItem object = listOfItems.getSelectedValue();
+				itemDescriptions.setText(object.getDescription());
+				// image.setIcon(ob.getImage());
 
-      }
-    });
-  }
+			}
+		});
+	}
 
-  public DefaultListModel<GameItem> getItems() {
-    return this.listModel;
-  }
+	public void addItemToInventory(GameItem item) {
+		listModel.addElement(item);
+	}
+
+	public DefaultListModel<GameItem> getItems() {
+		return this.listModel;
+	}
 }
