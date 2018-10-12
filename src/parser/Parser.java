54aadb217993a7bf18a868c729f927b0dfb0dd35
Vogog <vogog2@gmail.com>
2018-10-12 14:47:09 +1300

    Cleaned up the Parser class code.

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -21,208 +21,89 @@ public class Parser {
   public static final String TRUECHECKER = "true";
   public static final String FALSECHECKER = "false";
 
-  public static final String NORTHWALLCHECKER = "hasNorthWall";
-  public static final String SOUTHWALLCHECKER = "hasSouthWall";
-  public static final String EASTWALLCHECKER = "hasEastWall";
-  public static final String WESTWALLCHECKER = "hasWestWall";
-  public static final String DOORCHECKER = "door";
-  public static final String CONTAINSOBJECTSCHECKER = "hasObjects";
-
   private ArrayList<TestingSegment> listOfSegmentRooms = new ArrayList<>();
   private Segment[][] segments = new Segment[3][3];
 
-  /*
-  public void loadMap(File mapFile) {
-    listOfSegmentRooms = new ArrayList<>();
-    try {
-      BufferedReader buffer = new BufferedReader(new FileReader(mapFile));
-      String header = buffer.readLine();
-      String line = buffer.readLine();
-
-      while (line != null) {
-        // If we are loading the line signaling the start or end of a room description,
-        // do not read anything and move on.
-        if (!line.contains("/Room")) {
-          // If we are reading a room.
-          if (line.equals("<Room>")) {
-            TestingSegment newSegment = new TestingSegment();
-            line = buffer.readLine();
-
-            while (line != null) {
-              String sections[] = line.split(" ");
-              // Read if walls in four cardinal directions are present or not.
-              if (sections[0].contains(NORTHWALLCHECKER)) { // North Wall
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasNorthWall(true);
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasNorthWall(false);
-                } else {
-                  System.out.println("Invalid specification of wall being present.");
-                }
-              } else if (sections[0].contains(SOUTHWALLCHECKER)) { // South Wall
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasSouthWall(true);
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasSouthWall(false);
-                } else {
-                  System.out.println("Invalid specification of wall being present.");
-                }
-              } else if (sections[0].contains(EASTWALLCHECKER)) { // East Wall
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasEastWall(true);
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasEastWall(false);
-                } else {
-                  System.out.println("Invalid specification of wall being present.");
-                }
-              } else if (sections[0].contains(WESTWALLCHECKER)) { // West Wall
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasWestWall(true);
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasWestWall(false);
-                } else {
-                  System.out.println("Invalid specification of wall being present.");
-                }
-              }
-
-              // Read if a door is present, and if so, where is it located.
-              else if (sections[0].contains(DOORCHECKER)) {
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasDoor(true);
-                  newSegment.setDoorLocation(sections[2]);
-                  if (sections[3].equals("locked")) {
-                    newSegment.setDoorLocked(true);
-                  } else if (sections[3].equals("unlocked")) {
-                    newSegment.setDoorLocked(false);
-                  } else {
-                    System.out.println("Invalid specification of door being locked.");
-                  }
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasDoor(false);
-                } else {
-                  System.out.println("Invalid specification of door being present.");
-                }
-              }
-
-              // Read if there are objects present or not.
-              else if (sections[0].contains(CONTAINSOBJECTSCHECKER)) {
-                if (sections[1].equals(TRUECHECKER)) {
-                  newSegment.setHasObjects(true);
-                  line = buffer.readLine();
-                  sections = line.split(" ");
-                  if (sections[0].equals("<objects>")) {
-                    int i = 1;
-                    // Until the end of the line of objects, read the objects into the list of
-                    // objects
-                    while (!sections[i].equals("</objects>")) {
-                      newSegment.addObject(sections[i++]);
-                    }
-                  }
-                } else if (sections[1].equals(FALSECHECKER)) {
-                  newSegment.setHasObjects(false);
-                } else {
-                  System.out.println("Invalid specification of objects being present.");
-                }
-              }
-
-              line = buffer.readLine();
-            }
-            listOfSegmentRooms.add(newSegment);
-          }
-        }
-        line = buffer.readLine();
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("Error file not found.");
-    } catch (IOException e) {
-      System.out.println("IO Exception.");
-    }
-    for (TestingSegment s : listOfSegmentRooms) {
-
-      System.out.println("North Wall: " + s.hasNorthWall);
-      System.out.println("South Wall: " + s.hasSouthWall);
-      System.out.println("East Wall: " + s.hasEastWall);
-      System.out.println("West Wall: " + s.hasWestWall);
-      System.out.println("Door: " + s.hasDoor + ", Location: " + s.doorLocation + ", Locked: " + s.isDoorLocked);
-      System.out.println();
-      System.out.println("Objects:");
-      for (String obj : s.getObjects()) {
-        System.out.println(obj);
-      }
-    }
-  }*/
-
+  /**
+   * Method to used to parse the segments into a
+   * two dimensional array of Segments.
+   *
+   * @param mapFile
+   *          The source file storing the segment information.
+   * @return
+   *          The 2d array of segments storing map information.
+   */
   public Segment[][] loadMap(File mapFile) {
     try {
       XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 
       InputStream in = new FileInputStream(mapFile);
       XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
-        GameObject gameObjectType = null;
-        int x = 0;
-        int y = 0;
-        boolean hasPlayer = false;
-        Key key = null;
-        Door door = null;
+      //Create variables to store the information of the segments before creating them.
+      GameObject gameObjectType = null;
+      int x = 0;
+      int y = 0;
+      boolean hasPlayer = false;
+      Key key = null;
+      Door door = null;
       while (eventReader.hasNext()) {
-        XMLEvent event = eventReader.nextEvent();
-
-
-        if (event.isStartElement()) {
-          StartElement startElement = event.asStartElement();
-          //If we have found a room, we start the room parsing.
-          if (startElement.getName().getLocalPart().equals("NewSegment")) {
-              gameObjectType = null;
-              x = 0;
-              y = 0;
+      XMLEvent event = eventReader.nextEvent();
+      if (event.isStartElement()) {
+        StartElement startElement = event.asStartElement();
+        //If we have found a segment, we start the segment parsing.
+        if (startElement.getName().getLocalPart().equals("NewSegment")) {
+          gameObjectType = null;
+          x = 0;
+          y = 0;
+          hasPlayer = false;
+          key = null;
+          door = null;
+        }
+        if (event.asStartElement().getName().getLocalPart().equals("GameObject")) {
+          event = eventReader.nextEvent();
+          //Parse the appropriate GameObject based on the present tile
+          if (event.asCharacters().getData().equals("FreeSpaceTile")) {
+            gameObjectType = new FreeSpaceTile();
+          }
+          else if (event.asCharacters().getData().equals("Wall")) {
+            gameObjectType = new Wall();
+          }
+          //Doors also require additional information to be parsed.
+          else if (event.asCharacters().getData().equals("Door")) {
+            door = parseDoor(eventReader);
+            gameObjectType = door;
+          }
+          //Chests also require a key object to be created and stored within the chest.
+          else if (event.asCharacters().getData().equals("Chest")) {
+            key = parseKey(eventReader);
+            gameObjectType = new Chest(key);
+          }
+          continue;
+          }
+          //After parsing the Tile type, parse the coordinates.
+          if (event.asStartElement().getName().getLocalPart().equals("CoordinateX")) {
+            event = eventReader.nextEvent();
+            x = Integer.parseInt(event.asCharacters().getData());
+            continue;
+          }
+          if (event.asStartElement().getName().getLocalPart().equals("CoordinateY")) {
+            event = eventReader.nextEvent();
+            y = Integer.parseInt(event.asCharacters().getData());
+            continue;
+          }
+          //Parse the information declaring if the tile contains the player or not.
+          if (event.asStartElement().getName().getLocalPart().equals("hasPlayer")) {
+            event = eventReader.nextEvent();
+            if (event.asCharacters().getData().equals(TRUECHECKER)) {
+              hasPlayer = true;
+            }
+            else {
               hasPlayer = false;
-              key = null;
-              door = null;
+            }
+            continue;
           }
-            if (event.asStartElement().getName().getLocalPart().equals("GameObject")) {
-              event = eventReader.nextEvent();
-
-              if (event.asCharacters().getData().equals("FreeSpaceTile")) {
-                  gameObjectType = new FreeSpaceTile();
-              }
-              else if (event.asCharacters().getData().equals("Wall")) {
-                  gameObjectType = new Wall();
-              }
-              else if (event.asCharacters().getData().equals("Door")) {
-                door = parseDoor(eventReader);
-                gameObjectType = door;
-                //gameObjectType = new Door(101, false);
-              }
-              else if (event.asCharacters().getData().equals("Chest")) {
-                key = parseKey(eventReader);
-                gameObjectType = new Chest(key);
-              }
-              continue;
-            }
-
-            if (event.asStartElement().getName().getLocalPart().equals("CoordinateX")) {
-              event = eventReader.nextEvent();
-              x = Integer.parseInt(event.asCharacters().getData());
-              continue;
-            }
-
-            if (event.asStartElement().getName().getLocalPart().equals("CoordinateY")) {
-              event = eventReader.nextEvent();
-              y = Integer.parseInt(event.asCharacters().getData());
-              continue;
-            }
-
-            if (event.asStartElement().getName().getLocalPart().equals("hasPlayer")) {
-              event = eventReader.nextEvent();
-              if (event.asCharacters().getData().equals(TRUECHECKER)) {
-                hasPlayer = true;
-              }
-              else {
-                hasPlayer = false;
-              }
-              continue;
-            }
-          }
+        }
+        //Finally, after collecting all the appropriate information, create and store the segment.
         if (event.isEndElement()) {
           EndElement endElement = event.asEndElement();
           if (endElement.getName().getLocalPart().equals("NewSegment")) {
@@ -231,7 +112,7 @@ public class Parser {
             segments[x][y] = segment;
           }
         }
-        }
+      }
     }
     catch (IOException e) {
       System.out.println(e);
@@ -242,6 +123,13 @@ public class Parser {
     return segments;
   }
 
+  /**
+   * Helper method to parse key object information.
+   *
+   * @param eventReader
+   *          The XMLEventReader used for parsing.
+   * @return
+   */
   public Key parseKey(XMLEventReader eventReader) {
     int id = 0;
     String description = "";
@@ -285,6 +173,13 @@ public class Parser {
     return null;
   }
 
+  /**
+   * Helper method to parse Door object information.
+   *
+   * @param eventReader
+   *          The XMLEventReader used for parsing.
+   * @return
+   */
   public Door parseDoor(XMLEventReader eventReader) {
     int id = 0;
     boolean unlocked  = false;
