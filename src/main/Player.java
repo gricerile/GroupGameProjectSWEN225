ccb2106aa07553855def8a9f94b3acb04ec2dd3e
Riley Grice <gricerile@taputeranga.ecs.vuw.ac.nz>
2018-10-16 19:18:55 +1300

    java doc is now included in all main pakage

--- src/main/Player.java
+++ src/main/Player.java
@@ -3,33 +3,62 @@ package main;
 import java.util.ArrayList;
 
 public class Player {
-	// fields
-	private ArrayList<GameItem> inventory = new ArrayList<GameItem>();
-	private Segment segment;
+  // fields
+  private ArrayList<GameItem> inventory = new ArrayList<GameItem>();
+  private Segment segment;
 
-	public Player(Segment s) {
-		this.segment = s;
-	}
+  /**
+   * Constructor of Player.
+   *
+   * @param segment the player will spawn on.
+   */
+  public Player(Segment s) {
+    this.segment = s;
+  }
 
-	public Segment getSegment() {
-		return this.segment;
-	}
+  /**
+   * gets the segment the player is on.
+   *
+   * @return segment field.
+   */
+  public Segment getSegment() {
+    return this.segment;
+  }
 
-	public void move(Segment s) {
-		this.segment = s;
-	}
+  /**
+   * sets segment field to new segment.
+   *
+   * @param new segment
+   */
+  public void move(Segment s) {
+    this.segment = s;
+  }
 
-	public ArrayList<GameItem> getInventory() {
-		return this.inventory;
-	}
+  /**
+   * gets inventory of the player.
+   *
+   * @return the inventory field.
+   */
+  public ArrayList<GameItem> getInventory() {
+    return this.inventory;
+  }
 
-	public String giveKey(Key k, Main m) {
-		if (!this.inventory.contains(k)) {
-			this.inventory.add(k);
-			m.getGUI().getFrame().getInventoryPanel().addItemToInventory(k);
-			return "Key "+ k.getID() +" is the Inventory.";
-		}
-		return "The Key is already in the Inventory.";
-	}
+  /**
+   * Gives only new keys into the inventory.
+   *
+   *@param new key
+   *
+   *@param main to alter inventory event panel.
+   *
+   * @return String of events that happened when attempting to add key.
+   */
+  public String giveKey(Key k, Main m) {
+    if (!this.inventory.contains(k)) {
+      this.inventory.add(k);
+      m.getGUI().getFrame().getInventoryPanel().addItemToInventory(k);
+      return "Key " + k.getID() + " is the Inventory.";
+    }
+    return "The Key is already in the Inventory.";
+  }
 
 }
--- src/main/Segment.java
+++ src/main/Segment.java
@@ -5,61 +5,113 @@ import java.awt.Image;
 import renderer.Texture;
 
 public class Segment {
-	//fields
-	private GameObject object;
-	private int x,y;
-	private Image image;
-	private Texture texture = new Texture();
-	private boolean hasPlayer;
+  // fields
+  private GameObject object;
+  private int x, y;
+  private Image image;
+  private Texture texture = new Texture();
+  private boolean hasPlayer;
 
-	public Segment(GameObject object, int x,int y) {
-		this.object=object;
-		this.x=x;
-		this.y=y;
-		this.hasPlayer = false;
-	}
+  /**
+   * Constructor of Segment
+   *
+   * @param the game object type on the segment.
+   *
+   * @param x position.
+   *
+   * @param y position.
+   */
+  public Segment(GameObject object, int x, int y) {
+    this.object = object;
+    this.x = x;
+    this.y = y;
+    this.hasPlayer = false;
+  }
 
-	public boolean hasPlayer() {
-		return this.hasPlayer;
-	}
+  /**
+   * Gets the boolean of the has player field.
+   *
+   * @return boolean of has player field.
+   *
+   */
+  public boolean hasPlayer() {
+    return this.hasPlayer;
+  }
 
-	public void setHasPlayer(boolean b) {
-		 this.hasPlayer = b;
-	}
+  /**
+   * sets has player field.
+   *
+   *@param boolean which determines is has player field is true or false.
+   */
+  public void setHasPlayer(boolean b) {
+    this.hasPlayer = b;
+  }
 
-	public GameObject getObject(){
-		return this.object;
-	}
+  /**
+   * gets the game objects.
+   *
+   *@return the game object on the segment.
+   */
+  public GameObject getObject() {
+    return this.object;
+  }
 
-	public int getX() {
-		return this.x;
-	}
+  /**
+   * gets x coord.
+   *
+   *@return x coord of segment.
+   */
+  public int getX() {
+    return this.x;
+  }
 
-	public int getY() {
-		return this.y;
-	}
+  /**
+   * gets y coord.
+   *
+   *@return y coord of segment.
+   */
+  public int getY() {
+    return this.y;
+  }
 
-	public boolean equals(Segment s) {
-		if(this.getX()==s.getX() && this.getY()==s.getY()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  /**
+   * Method to compare two segments to check if they're the same.
+   *
+   * @param segment that will be compared.
+   *
+   *@return boolean of comparison.
+   */
+  public boolean equals(Segment s) {
+    if (this.getX() == s.getX() && this.getY() == s.getY()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	public String opensChest() {
-		if(this.object instanceof Chest) {
-			Chest c = (Chest) this.object;
-			return c.openAndClose();
-		}
-		return "";
-	}
+  /**
+   *Opens chest on segment if there is a chest.
+   *
+   *@return String from the event of the opening or not of the chest.
+   */
+  public String opensChest() {
+    if (this.object instanceof Chest) {
+      Chest c = (Chest) this.object;
+      return c.openAndClose();
+    }
+    return "";
+  }
 
-	public Key takeFromChest() {
-		if(this.object instanceof Chest) {
-			Chest c = (Chest) this.object;
-			return c.takeKey();
-		}
-		return null;
-	}
+  /**
+   * gets the key from a chest and returns it.
+   *
+   *@return key from chest.
+   */
+  public Key takeFromChest() {
+    if (this.object instanceof Chest) {
+      Chest c = (Chest) this.object;
+      return c.takeKey();
+    }
+    return null;
+  }
 }
